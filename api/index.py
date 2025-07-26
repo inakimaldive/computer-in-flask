887b877105bfeeda500d93b3de74941308d6ac7c
Your Name <maldive@protonmail.com>
2025-07-26 12:18:19 +0200

    htmx date and landing

--- api/index.py
+++ api/index.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('landing.html')
 
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/time')
+def get_time():
+    from datetime import datetime
+    return f"<div>{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}</div>"
+
+@app.route('/time-page')
+def time_page():
+    return render_template('time.html')
